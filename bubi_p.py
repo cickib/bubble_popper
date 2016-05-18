a6efb18637d19a68e2b73777027752172190547e
cickib <bellakocsis06@gmail.com>
2016-05-18 13:21:15 +0200

    sd

--- bubi_p.py
+++ bubi_p.py
@@ -1,16 +1,26 @@
 import curses
 import time
-stdscr = curses.initscr()
+import random
+screen = curses.initscr()
 
 printable_num = list(c for c in range(48, 58))
-print(printable_num)
-print(type(printable_num))
+printable_num.append(27)
+
+rand_num = 0
+def newNum():
+    global rand_num
+    rand_num = random.randint(0, 9)
+    num_y = 0
+    num_x = 0
+    screen.addstr(num_y, num_x, str(rand_num))
+    #screen.move(10 ,10)
+    print(rand_num)
 
 
 def popping():
+    global rand_num
     curses.noecho()
     curses.curs_set(0)
-
     begin_x = 0
     begin_y = 0
     height = 100
@@ -21,17 +31,32 @@ def popping():
     win.nodelay(1)
     title = "Bubble Popper"
     win.addstr(0, (curses.COLS - len(title)) // 2, title) #ide rakja a feliratot, középre igazítva
+    num_y = 0
+    num_x =0
 
 
     key = 48
 
-    while key != 27:            # not Esc is pressed
-    #        win.timeout(100)        # wait 0.1 sec
-        event = win.getch()
-        if event in printable_num:
-            key = event
-            print(key)
+    while key != 27:
+        global rand_num
+        answer = key - 48
+        win.timeout(700)
+        key = win.getch()
+        if key == printable_num[len(printable_num)-1]:
+            break
+        elif key == answer:
+            screen.addstr("The User Pressed Lower Case p")
+        #if rand_num == answer:
+        #    win.clear()
+        #event = screen.getch()
 
+        newNum()
+
+        if key in printable_num:
+            key = event
+            #if event
+            #print(answer) - csak megvizsg.
+        #if event ==
 
     #key = 48
 #        snake = [[4,10]]
@@ -61,9 +86,9 @@ curses.cbreak()
 #the initscr() function, which will determine the terminal type, send any
 #required setup codes to the terminal, and create various internal
 #data structures. If successful, initscr() returns a window object representing
-#the entire screen; this is usually called stdscr after the name of the
+#the entire screen; this is usually called screen after the name of the
 #corresponding C variable.
-stdscr = curses.initscr()
+screen = curses.initscr()
 
 #window
 begin_x = 0
