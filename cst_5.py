a6301997e8cacdcc4512009cde68fb42172e42e8
cickib <bellakocsis06@gmail.com>
2016-05-19 19:48:21 +0200

    fancy szivecskés és működő

--- cst_5.py
+++ cst_5.py
@@ -5,7 +5,8 @@ from curses import wrapper
 import rocket
 
 def main(screen):
-
+    t_out = 250
+    insane_mode = False
     key = 48
     score = 0
     life = 3
@@ -21,29 +22,54 @@ def main(screen):
     win.keypad(1)
     win.border(0)
     win.nodelay(1)
-    title = "Bubble Popper"
-    win.addstr(0, (width - len(title)) // 2, title) #nem műkszik
-    score_str = "Score: " + str(score)
-    win.addstr(1, 5, score_str)
+    title = "<< Bubble Popper >>"
+    win.addstr(0, (width - len(title)) // 2, title)
+
     win.refresh()
 
     printable_num = list(c for c in range(48, 58))
     printable_num.append(27)
 
+    life_heart = "💙 "
+    life_lost = "💔 "
+
+    #life_str = "Life:  " + str(life_heart*3)
+    win.addstr(1, 5, "Life:  ")
+    win.addstr(1, 12, life_heart)
+    win.addstr(1, 14, life_heart)
+    win.addstr(1, 16, life_heart)
+
     while key != 27:
-        life_str = "Life:  " + str("💙 "*life)
-        win.addstr(1, 5, life_str)
         score_str = "Score: " + str(score)
         win.addstr(2, 5, score_str)
+        if life == 2:
+            win.addstr(1, 12, life_lost)
+        if life == 1:
+            win.addstr(1, 14, life_lost)
+        if life == 0:
+            win.addstr(1, 16, life_lost)
+            win.clear()
+            game_over = "Your score is " + str(score)
+            win.addstr(15, 40, game_over)
+            score_str = "Score: " + str(score)
+            win.addstr(2, 5, score_str)
+            break
+
         if score == 0:
             win.addstr(1, 45, "Press the right number for points.")
             win.addstr(2, 45, "If you miss one, you lost a life.")
         else:
-            win.addstr(1, 45, (" "*35))
+            win.addstr(1, 45, (" "*34))
             win.addstr(2, 45, (" "*33))
-        if score == 5:
-            exit()
+        if t_out < 51:
+            insane_mode = True
+            win.addstr(1, 45, ("Insade mode: ON"))
+        if score % 10 == 0:
+            t_out -=50
+        #if score == 20:
+        #    exit()
         win.refresh()
+
         if count % 10 == 0 :
             x = random.randint(0, 9)
             num_y = 3
@@ -53,7 +79,7 @@ def main(screen):
             win.addstr(num_y+2, num_x, str(rocket.rockets[x][2]))
             while num_y != (height-4) or ((event-48) == x):
                 win.refresh()
-                win.timeout(100)
+                win.timeout(t_out)
                 num_y += 1
                 win.addstr(num_y-1, num_x, "     ")
                 win.addstr(num_y, num_x, str(rocket.rockets[x][0]))
@@ -63,17 +89,23 @@ def main(screen):
                 if event == printable_num[len(printable_num)-1]:
                     exit()
                 if (event-48) == x:
-                    score += 1
+                    if insane_mode == False:
+                        score += 1
+                    else:
+                        score += 5
                     win.addstr(num_y, num_x, "     ")
                     win.addstr(num_y+1, num_x, "     ")
                     win.addstr(num_y+2, num_x, "     ")
                     break
+
                 win.addstr(num_y, num_x, "     ")
                 win.addstr(num_y+1, num_x, "     ")
                 win.addstr(num_y+2, num_x, "     ")
+            else:
                 life -= 1
             continue
-        win.timeout(100)
+        win.timeout(t_out)
         count += 1
+
 
 curses.wrapper(main)
