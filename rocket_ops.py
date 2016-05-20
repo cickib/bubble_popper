f57dd8a2a4f3994071dc0e030a0aa995436e3c61
Bella Kocsis <bellakocsis06@gmail.com>
2016-05-20 09:38:26 +0200

    cleaning up the repo

--- rocket_ops.py
+++
@@ -1,9 +0,0 @@
-import random
-import curses
-num_1 = random.randint(0, 9)
-num_2 = random.randint(0, 9)
-ops = ["+", "-"]
-muv = []
-rocket_op = [num_1, ops[random.randint(0,1)], num_2]
-for o in range(0, (len(rocket_op))):
-    muv.append(rocket_op[o])
